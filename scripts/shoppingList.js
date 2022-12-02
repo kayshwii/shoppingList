f1b3dfd9f433353e9cc08b29266154ab631ad127
kayshwii <kayshwii@icloud.com>
2022-12-02 17:34:12 -0600

    added localstorage

--- scripts/shoppingList.js
+++ scripts/shoppingList.js
@@ -1,10 +1,18 @@
 const addButton = document.getElementById('addEntry');
 const enterItem = document.getElementById('enterItem');
 const shoppingList = document.getElementById('shoppingList');
+const itemList = [];
+
 
 addButton.addEventListener('click', function(){
-    const item = document.createElement('li');
-    item.innerText = enterItem.value;
-    shoppingList.appendChild(item);
+    itemList.push(enterItem.value)
+    const li = document.createElement('li');
+    const text = document.createTextNode(enterItem.value);
+    li.appendChild(text);
+    shoppingList.appendChild(li);
+    localStorage.setItem('items', shoppingList.innerHTML)
+    console.log(itemList);
     enterItem.value = '';
 })
+
+if (localStorage['items']){shoppingList.innerHTML = localStorage.getItem('items')}
