ffcc144c8bcbcf2d8fbdaf2133f0c1271bee7931
kayshwii <kayshwii@icloud.com>
2022-12-02 20:55:25 -0600

    added checkbox to add/remove strike

--- scripts/shoppingList.js
+++ scripts/shoppingList.js
@@ -5,14 +5,17 @@ const shoppingList = document.getElementById('shoppingList');
 
 addButton.addEventListener('click', function(){
     const li = document.createElement('li');
+    const checkButton = document.createElement('input')
+    checkButton.setAttribute('type', 'checkbox')
+    checkButton.setAttribute('onclick', 'strike()');
     const delButton = document.createElement('button');
     const delButtonX = document.createTextNode('x');
     const text = document.createTextNode(enterItem.value);
     delButton.setAttribute('onclick', 'remove()');
     delButton.appendChild(delButtonX);
     li.appendChild(text);
+    li.appendChild(checkButton)
     li.appendChild(delButton);
-    li.setAttribute('onclick', 'strike()');
     shoppingList.appendChild(li);
     localStorage.setItem('items', shoppingList.innerHTML)
     enterItem.value = '';
@@ -24,8 +27,13 @@ function remove(){
 }
 
 function strike(){
-    const line = this.event.currentTarget;
-    line.style.textDecoration = 'line-through';
+    const line = this.event.currentTarget.parentNode;
+    const box = this.event.currentTarget;
+    if (box.checked == true){
+        line.style.textDecoration = "line-through"
+    } else {
+        line.style.textDecoration = null;
+    }
 }
 
 if (localStorage['items']){
