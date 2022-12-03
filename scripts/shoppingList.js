78e45bc31f150596b84bcf6898186bf3aed2204d
kayshwii <kayshwii@icloud.com>
2022-12-02 21:16:45 -0600

    removed checkbox. fixed old strike function

--- scripts/shoppingList.js
+++ scripts/shoppingList.js
@@ -5,17 +5,14 @@ const shoppingList = document.getElementById('shoppingList');
 
 addButton.addEventListener('click', function(){
     const li = document.createElement('li');
-    const checkButton = document.createElement('input')
-    checkButton.setAttribute('type', 'checkbox')
-    checkButton.setAttribute('onclick', 'strike()');
     const delButton = document.createElement('button');
     const delButtonX = document.createTextNode('x');
     const text = document.createTextNode(enterItem.value);
     delButton.setAttribute('onclick', 'remove()');
     delButton.appendChild(delButtonX);
     li.appendChild(text);
-    li.appendChild(checkButton)
     li.appendChild(delButton);
+    li.setAttribute('onclick', 'strike()');
     shoppingList.appendChild(li);
     localStorage.setItem('items', shoppingList.innerHTML)
     enterItem.value = '';
@@ -27,12 +24,11 @@ function remove(){
 }
 
 function strike(){
-    const line = this.event.currentTarget.parentNode;
-    const box = this.event.currentTarget;
-    if (box.checked == true){
-        line.style.textDecoration = "line-through"
+    const line = this.event.currentTarget;
+    if (line.style.textDecoration === 'none'){
+        line.style.textDecoration = 'line-through';
     } else {
-        line.style.textDecoration = null;
+        line.style.textDecoration = 'none';
     }
 }
 
