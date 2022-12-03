df842d9e8f595e4328411a2569040d1c033efce7
kayshwii <kayshwii@icloud.com>
2022-12-02 20:26:46 -0600

    added strikethrough on item click

--- scripts/shoppingList.js
+++ scripts/shoppingList.js
@@ -1,11 +1,9 @@
 const addButton = document.getElementById('addEntry');
 const enterItem = document.getElementById('enterItem');
 const shoppingList = document.getElementById('shoppingList');
-const itemList = [];
 
 
 addButton.addEventListener('click', function(){
-    itemList.push(enterItem.value)
     const li = document.createElement('li');
     const delButton = document.createElement('button');
     const delButtonX = document.createTextNode('x');
@@ -14,16 +12,20 @@ addButton.addEventListener('click', function(){
     delButton.appendChild(delButtonX);
     li.appendChild(text);
     li.appendChild(delButton);
+    li.setAttribute('onclick', 'strike()');
     shoppingList.appendChild(li);
     localStorage.setItem('items', shoppingList.innerHTML)
-    console.log(itemList);
     enterItem.value = '';
 })
 
 function remove(){
     const button = this.event.currentTarget.parentNode;
     shoppingList.removeChild(button);
-    localStorage.setItem('items', shoppingList.innerHTML)
+}
+
+function strike(){
+    const line = this.event.currentTarget;
+    line.style.textDecoration = 'line-through';
 }
 
 if (localStorage['items']){
