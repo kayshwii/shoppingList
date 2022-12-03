6daa0e578ccfbf795302cd7bfdae79600908a461
kayshwii <kayshwii@icloud.com>
2022-12-02 18:40:32 -0600

    added remove function

--- scripts/shoppingList.js
+++ scripts/shoppingList.js
@@ -7,12 +7,25 @@ const itemList = [];
 addButton.addEventListener('click', function(){
     itemList.push(enterItem.value)
     const li = document.createElement('li');
+    const delButton = document.createElement('button');
+    const delButtonX = document.createTextNode('x');
     const text = document.createTextNode(enterItem.value);
+    delButton.setAttribute('onclick', 'remove()');
+    delButton.appendChild(delButtonX);
     li.appendChild(text);
+    li.appendChild(delButton);
     shoppingList.appendChild(li);
     localStorage.setItem('items', shoppingList.innerHTML)
     console.log(itemList);
     enterItem.value = '';
 })
 
-if (localStorage['items']){shoppingList.innerHTML = localStorage.getItem('items')}
+function remove(){
+    const button = this.event.currentTarget.parentNode;
+    shoppingList.removeChild(button);
+    localStorage.setItem('items', shoppingList.innerHTML)
+}
+
+if (localStorage['items']){
+    shoppingList.innerHTML = localStorage.getItem('items')
+}
